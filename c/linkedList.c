42f61f214fca18504c23a85ca9477464e60e62ff
dooooooooinggggg <dooooooooinggggg@gmail.com>
2018-09-18 14:03:15 +0900

    Sloppy Commit

---
+++ c/linkedList.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Linked List
+struct listStruct
+{
+    char value[20];
+    struct listStruct *next;
+};
+
+void printLinkedList(struct listStruct *thisList)
+{
+    printf("this value is [%s]\n", thisList->value);
+    if (thisList->next == NULL)
+        return;
+    return printLinkedList(thisList->next);
+}
+
+void AddValueToList(struct listStruct *linkedList, char value[20])
+{
+    if (linkedList->next != NULL)
+        return AddValueToList(linkedList->next, value);
+
+    // それ以降
+    struct listStruct *thisList = (struct listStruct *)malloc(sizeof(struct listStruct));
+    strcpy(thisList->value, value);
+    thisList->next = NULL;
+    linkedList->next = thisList;
+    return;
+}
+
+void initList(struct listStruct *linkedList, char value[20])
+{
+    strcpy(linkedList->value, value);
+    linkedList->next = NULL;
+    return;
+}
+
+int main()
+{
+    struct listStruct *linkedList;
+    initList(linkedList, "aa");
+    AddValueToList(linkedList, "bb");
+    AddValueToList(linkedList, "cc");
+    printLinkedList(linkedList);
+    return 1;
+}
